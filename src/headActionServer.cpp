2dc02aedccd25ca057885a1b21550a2df6d3b8b4
passionbull <wnsl5684@gmail.com>
2020-05-15 16:24:58 +0900

    updated feedback info

--- src/headActionServer.cpp
+++ src/headActionServer.cpp
@@ -33,23 +33,39 @@ private:
   std::vector< std::string> joint_names_;
   ros::ServiceClient cli_query_traj_;
 
+  control_msgs::JointTrajectoryControllerStateConstPtr last_controller_state_;
+
+
 public:
   
 
   ros::NodeHandle nh_;
-  PHAS action_server_;
   std::string action_name_;
   KDL::Tree tree_;
   KDL::Chain chain_;
   urdf::Model urdf_model_;
 
+  PHAS action_server_;
+  bool has_active_goal_;
+  GoalHandle active_goal_;
+  double success_angle_threshold_; //one of the stop conditions in the iterative solver
+
+  boost::scoped_ptr<KDL::ChainFkSolverPos> pose_solver_;
+  boost::scoped_ptr<KDL::ChainJntToJacSolver> jac_solver_;
+  tf::Vector3 pointing_axis_;
+  tf::Point target_in_root_;
+  tf::Vector3 desired_pointing_axis_in_frame_;
+  double goal_error_; //this may be larger than success_angle_threshold_
+
   ros::Publisher pub_controller_command_;
+  ros::Subscriber sub_controller_state_;
 
   HeadAction(const ros::NodeHandle &node):
   	action_name_("absolute_point_head_action"), nh_(node),
   	action_server_(nh_, "absolute_point_head_action",
                        boost::bind(&HeadAction::goalCB, this, _1),
-                       boost::bind(&HeadAction::cancelCB, this, _1), false)
+                       boost::bind(&HeadAction::cancelCB, this, _1), false),
+    has_active_goal_(false)
   {
     if (tree_.getNrOfJoints() == 0)
     {
@@ -79,6 +95,8 @@ public:
     // Connects to the controller
     pub_controller_command_ =
       nh_.advertise<trajectory_msgs::JointTrajectory>("command", 2);
+    sub_controller_state_ =
+      nh_.subscribe("state", 1, &HeadAction::controllerStateCB, this);
 
     cli_query_traj_ =
         nh_.serviceClient<control_msgs::QueryTrajectoryState>("query_state");
@@ -87,12 +105,9 @@ public:
   void goalCB(GoalHandle gh)
   {
 
-    boost::scoped_ptr<KDL::ChainFkSolverPos> pose_solver_;
-    boost::scoped_ptr<KDL::ChainJntToJacSolver> jac_solver_;
     double success_angle_threshold_ = 0.03;
 
     const geometry_msgs::PointStamped &target = gh.getGoal()->target;
-    tf::Point target_in_root_;
     target_in_root_[0] = target.point.x;
     target_in_root_[1] = target.point.y;
     target_in_root_[2] = target.point.z;
@@ -132,7 +147,6 @@ public:
       jnt_pos(i) = 0;
     }
 
-    ROS_INFO("He11o 2");
 
     int count = 0;
     int limit_flips = 0;
@@ -153,7 +167,6 @@ public:
       tf::Transform frame_in_root;
       tf::poseKDLToTF(pose, frame_in_root);
 
-      tf::Vector3 pointing_axis_;
       tf::vector3MsgToTF(gh.getGoal()->pointing_axis, pointing_axis_);
 
 
@@ -164,8 +177,8 @@ public:
       correction_angle = current_in_frame.angle(axis_in_frame);
       correction_delta = correction_angle - prev_correction;
 
-      ROS_INFO("At step %d, joint poses are %.4f and %.4f, angle error is %f radians", count, jnt_pos(0), jnt_pos(1), correction_angle);
-      double goal_error_ = correction_angle; //expected error after this iteration
+      // ROS_INFO("At step %d, joint poses are %.4f and %.4f, angle error is %f radians", count, jnt_pos(0), jnt_pos(1), correction_angle);
+      goal_error_ = correction_angle; //expected error after this iteration
       if ( correction_angle < 0.5*success_angle_threshold_ )
       {
         ROS_DEBUG_STREAM("Accepting solution as estimated error is: " << correction_angle*180.0/M_PI << "degrees and stopping condition is half of " << success_angle_threshold_*180.0/M_PI);
@@ -199,6 +212,18 @@ public:
         break;
       }
     }
+    if (has_active_goal_)
+    {
+      ROS_INFO("Don't pass the goal");
+      return;
+    }
+
+
+    gh.setAccepted();
+    active_goal_ = gh;
+    has_active_goal_ = true;
+
+
     std::vector<double> q_goal(joints);
     for(unsigned int i = 0; i < joints; i++)
       q_goal[i] = jnt_pos(i);
@@ -214,7 +239,7 @@ public:
     tf::poseKDLToTF(pose, frame_in_root);
     tf::Vector3 target_from_frame = target_in_root_ - frame_in_root.getOrigin();
     target_from_frame.normalize();
-    tf::Vector3 desired_pointing_axis_in_frame_ = frame_in_root.getBasis().inverse()*target_from_frame;
+    desired_pointing_axis_in_frame_ = frame_in_root.getBasis().inverse()*target_from_frame;
     ROS_INFO_STREAM("desired_pointing_axis_in_frame_ " <<desired_pointing_axis_in_frame_[0] <<", "<<desired_pointing_axis_in_frame_[1]<<", "<< desired_pointing_axis_in_frame_[2]);
 
     // Computes the duration of the movement.
@@ -256,8 +281,71 @@ public:
   }
   void cancelCB(GoalHandle gh)
   {
-    ROS_INFO_STREAM("cancelCB");
-  }  
+    if (active_goal_ == gh)
+    {
+      // Stops the controller.
+      trajectory_msgs::JointTrajectory empty;
+      empty.joint_names = joint_names_;
+      pub_controller_command_.publish(empty);
+
+      // Marks the current goal as canceled.
+      active_goal_.setCanceled();
+      has_active_goal_ = false;
+    } 
+  }
+
+  void controllerStateCB(const control_msgs::JointTrajectoryControllerStateConstPtr &msg)
+  {
+    last_controller_state_ = msg;
+    const ros::Time now = ros::Time::now();
+
+    if (!has_active_goal_)
+      return;
+
+    //! \todo Support frames that are not the pan link itself
+    try
+    {     
+
+      //now compute the current pointing axis from actual joint positions       
+      KDL::JntArray jnt_pos(msg->joint_names.size());
+      for (size_t i = 0; i < msg->joint_names.size(); ++i)
+      {
+        jnt_pos(i) = msg->actual.positions[i];
+        ROS_DEBUG_STREAM("current state of joint " << i << ": " << jnt_pos(i));
+      }
+
+      KDL::Frame pose;
+      pose_solver_->JntToCart(jnt_pos, pose);
+
+      tf::Transform frame_in_root;
+      tf::poseKDLToTF(pose, frame_in_root);
+
+      tf::Vector3 axis_in_frame = pointing_axis_.normalized();
+      tf::Vector3 target_from_frame = target_in_root_ - frame_in_root.getOrigin();
+
+      target_from_frame.normalize();
+      tf::Vector3 current_in_frame = frame_in_root.getBasis().inverse()*target_from_frame;
+
+      control_msgs::PointHeadFeedback feedback;
+      feedback.pointing_angle_error = current_in_frame.angle(desired_pointing_axis_in_frame_);
+
+      ROS_INFO_STREAM("current error is: " << feedback.pointing_angle_error << " radians");
+
+      active_goal_.publishFeedback(feedback);
+
+      if (feedback.pointing_angle_error <= 0.01)
+      {        
+        ROS_INFO_STREAM("goal succeeded with error: " << feedback.pointing_angle_error << " radians");
+        active_goal_.setSucceeded();
+        has_active_goal_ = false;
+      }
+      past_error_ = feedback.pointing_angle_error;
+    }
+    catch (const tf::TransformException &ex)
+    {
+      ROS_ERROR("Could not transform: %s", ex.what());
+    }
+  }
 };
 
 int main(int argc, char** argv)
